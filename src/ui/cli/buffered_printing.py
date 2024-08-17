1c077fa44188b8948c1801c346e3813f211eb4c9
Florian Bender <benderflorian70@gmail.com>
2024-08-17 16:32:41 +0200

    Allow printing in a well defined manner
    All prints are buffered and drawn at the end of the CLI UI's draw function.
    All prints outside the CLI UI are drawn below the drawn board.

---
+++ src/ui/cli/buffered_printing.py
@@ -0,0 +1,50 @@
+import io
+import sys
+from types import TracebackType
+from typing import Self
+
+
+class BufferedPrint:
+    """Redirection of stdout to a buffer which is printed to the console all at once at the desired time."""
+
+    def __init__(self) -> None:
+        self._buffer = io.StringIO()
+
+    def is_active(self) -> bool:
+        return sys.stdout is self._buffer
+
+    def start_buffering(self) -> Self:
+        if self.is_active():
+            raise RuntimeError("BufferedPrint is already active")
+
+        sys.stdout = self._buffer
+        return self
+
+    def print_and_reset_buffer(self) -> None:
+        if not self.is_active():
+            raise RuntimeError("BufferedPrint is not active")
+
+        sys.stdout = sys.__stdout__
+        print(self._buffer.getvalue(), end="")
+        self._buffer.close()
+        self._buffer = io.StringIO()
+
+    def discard_and_reset_buffer(self) -> None:
+        if not self.is_active():
+            raise RuntimeError("BufferedPrint is not active")
+
+        sys.stdout = sys.__stdout__
+        self._buffer.close()
+        self._buffer = io.StringIO()
+
+    def print_and_restart_buffering(self) -> None:
+        self.print_and_reset_buffer()
+        self.start_buffering()
+
+    def __enter__(self) -> Self:
+        return self.start_buffering()
+
+    def __exit__(
+        self, exc_type: type[BaseException] | None, exc_value: BaseException | None, traceback: TracebackType | None
+    ) -> None:
+        self.print_and_reset_buffer()
--- src/ui/cli/ui.py
+++ src/ui/cli/ui.py
@@ -1,16 +1,18 @@
 import atexit
-import sys
 from dataclasses import dataclass
-from typing import Literal, NamedTuple, Self
 
 import numpy as np
 from ansi import color, cursor
 from numpy.typing import NDArray
 
-from ansi_extensions import color as colorx
+from ansi_extensions import cursor as cursorx
+from ui.cli.buffered_printing import BufferedPrint
+from ui.cli.color_palette import ColorPalette
 
 # see https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797 for a list of ANSI escape codes,
 # NOT ALL of which are provided in the `ansi` package
+
+# NOTE: long-term, this should be split up into a general, reusable part, and a tetris-specific part
 
 
 @dataclass(frozen=True, slots=True)
@@ -22,53 +24,16 @@ class Vec:
         return Vec(self.y + other.y, self.x + other.x)
 
 
-class ColorPalette(NamedTuple):
-    board_bg: str
-    board_bg_alt: str
-    board_fg: str
-
-    @classmethod
-    def from_rgb(
-        cls,
-        board_bg: tuple[int, int, int],
-        board_bg_alt: tuple[int, int, int],
-        board_fg: tuple[int, int, int],
-        mode: Literal["palette", "truecolor"] = "truecolor",
-    ) -> Self:
-        color_fn = colorx.bg.rgb_palette if mode == "palette" else colorx.bg.rgb_truecolor
-        return cls(
-            board_bg=color_fn(*board_bg),
-            board_bg_alt=color_fn(*board_bg_alt),
-            board_fg=color_fn(*board_fg),
-        )
-
-    @classmethod
-    def index_of_color(
-        cls,
-        color_name: Literal[
-            "board_bg",
-            "board_bg_alt",
-            "board_fg",
-        ],
-    ) -> int:
-        try:
-            return cls._fields.index(color_name)
-        except ValueError as e:
-            raise ValueError(f"Invalid color name: '{color_name}'") from e
-
-
 class CLI:
     PIXEL_WIDTH = 2  # how many terminal characters together form one pixel
 
-    def __init__(
-        self,
-        color_palette: ColorPalette | None = None,
-    ) -> None:
+    def __init__(self, color_palette: ColorPalette | None = None) -> None:
         self._last_image_buffer: NDArray[np.uint8] | None = None
         self._board_background: NDArray[np.uint8] | None = None
         self._color_palette = color_palette or ColorPalette.from_rgb(
             board_bg=(80, 80, 80), board_bg_alt=(60, 60, 60), board_fg=(200, 200, 200)
         )
+        self._buffered_print = BufferedPrint()
 
     @staticmethod
     def _cursor_goto(vec: Vec) -> str:
@@ -76,18 +41,35 @@ class CLI:
         return cursor.goto(vec.y + 1, vec.x * CLI.PIXEL_WIDTH + 1)
 
     def initialize(self, board_height: int, board_width: int) -> None:
+        self._initialized_board_background(board_height, board_width)
+        self._initialize_terminal(board_height)
+
+    def _initialized_board_background(self, board_height: int, board_width: int) -> None:
         self._board_background = np.full((board_height, board_width), ColorPalette.index_of_color("board_bg"))
         self._board_background[1::2, ::2] = ColorPalette.index_of_color("board_bg_alt")
         self._board_background[::2, 1::2] = ColorPalette.index_of_color("board_bg_alt")
 
-        print(cursor.hide("") + cursor.erase(""), end="")
+    def _initialize_terminal(self, board_height: int) -> None:
         atexit.register(self.terminate)
+        self._initialize_cursor()
+        self._setup_cursor_for_normal_printing(board_height)
+        self._buffered_print.start_buffering()
+
+    def _initialize_cursor(self) -> None:
+        print(cursor.hide("") + cursor.erase(""), end="")
 
     def terminate(self) -> None:
+        if self._buffered_print.is_active():
+            self._buffered_print.discard_and_reset_buffer()
         print(color.fx.reset + cursor.erase("") + self._cursor_goto(Vec(0, 0)) + cursor.show(""), end="")
 
     def draw(self, board: NDArray[np.bool]) -> None:
-        assert self._board_background is not None, "draw() called before initialize()!"
+        assert (
+            self._board_background is not None
+        ), "background not initialized, likely draw() was called before initialize()!"
+        assert (
+            self._buffered_print.is_active()
+        ), "buffered printing not active, likely draw() called before initialize()!"
 
         image_buffer = np.where(board, ColorPalette.index_of_color("board_fg"), self._board_background)
 
@@ -97,13 +79,22 @@ class CLI:
             for y, x in zip(*(image_buffer != self._last_image_buffer).nonzero(), strict=True):
                 self._draw_pixel(Vec(y, x), color_index=int(image_buffer[y, x]))
 
-        sys.stdout.flush()  # make sure the changes are actually shown on screen
-
         self._last_image_buffer = image_buffer
 
+        self._buffered_print.print_and_restart_buffering()
+        self._setup_cursor_for_normal_printing(board_height=len(board))
+
+    def _setup_cursor_for_normal_printing(self, board_height: int) -> None:
+        """Move the cursor below the board, clear any colors, and erase anything below the board.
+
+        This allows usage of print outside the class in a well-defined manner, appearing below the board and being reset
+        every after every call to this function.
+        """
+        print(self._cursor_goto(Vec(board_height, 0)) + color.fx.reset + cursorx.erase_to_end(""))
+
     def game_over(self, board: NDArray[np.bool]) -> None:
+        print("Game Over")
         self.draw(board)
-        print(self._cursor_goto(Vec(len(board), 0)) + color.fx.reset + "Game Over")
 
     def _draw_array(self, top_left: Vec, array: NDArray[np.uint8]) -> None:
         for idx, row in enumerate(array):
