88740bee4a103a3fdf991a822008d6d24b17df10
Florian Bender <benderflorian70@gmail.com>
2024-08-20 19:07:18 +0200

    WIP great working algorithm, but probably still room for optimization

--- src/main.py
+++ src/main.py
@@ -48,7 +48,7 @@ def main() -> None:
 
 if __name__ == "__main__":
     # main()
-    space = np.zeros((32, 32), dtype=np.int16)
+    space = np.zeros((72, 72), dtype=np.int16)
     space[8:24, 8:24] = -1
     filler = TetrominoSpaceFiller(space)
     filler.fill()
