4e9cfffd472b3e153bcd1ce4c40a5f1f1673a122
Florian Bender <benderflorian70@gmail.com>
2024-09-08 20:08:23 +0200

    WIP initial attempt at FourColorizer
    In this first iteration, trying to color the blocks in the order in which they were placed (i.e. in ascending order of their values)

---
+++ src/tetris/tetromino_space_filler/four_colorizer.py
@@ -0,0 +1,250 @@
+import contextlib
+import inspect
+import sys
+from collections.abc import Callable, Generator, Iterator
+from itertools import chain
+
+import numpy as np
+from numpy.typing import NDArray
+
+from tetris.tetromino_space_filler.offset_iterables import CyclingOffsetIterable, RandomOffsetIterable
+
+
+class FourColorizer:
+    STACK_FRAMES_SAFETY_MARGIN = 10
+    NUM_COLORS = 4
+
+    def __init__(
+        self,
+        space: NDArray[np.int32],
+        *,
+        use_rng: bool = True,
+        rng_seed: int | None = None,
+        space_updated_callback: Callable[[], None] | None = None,
+        total_blocks_to_color: int | None = None,
+    ) -> None:
+        """Initialize the four-colorer.
+
+        Args:
+            space: The space to be colored. It will not be modified inplace. Values <= 0 are ignored and will not be
+                colored. Values > 0 are considered spots to be colored. All cells with the same value will be colored
+                the same. There should be no gaps in the contained values, i.e. if np.max(space) == N, then all values
+                in range(1, N + 1) should be contained in the space.
+                The resulting space will only have values from 0 to 4. It will be 0 wherever space <= 0, and 1 to 4
+                everywhere else, representing the four colors.
+
+            TODO: the following has yet to be adapted
+
+            use_rng: Whether to use randomness in the selection of color, selection of tetromino rotation/transpose,
+                and selection of neighboring spot to fill next.
+            rng_seed: Optional seed to use for all RNG.
+            top_left_tendency: Whether to have a slight bias towards selecting spots top left of the last placed
+                tetromino as the next spot to be filled. This causes a tendency for the algorithm to gravitate towards
+                the top left of the space. This makes the filling up more predictable, but also reduces the likelihood
+                of large backtracks becoming necessary because the "surface area" of tetrominos tends to be smaller.
+                If True, selection of neighboring spot to fill next will not be random, even if use_rng is True.
+            space_updated_callback: Callback function which is called each time after the space has been updated (a
+                tetromino has been placed or removed). This effectively temporarily hands control back to the user of
+                this class, letting it act one the latest space update (e.g. for drawing).
+        """
+        if not use_rng and rng_seed is not None:
+            msg = "rng_seed should only be set when use_rng is True"
+            raise ValueError(msg)
+
+        if np.max(space) <= 0:
+            msg = "Space must contain at least one positive value. Otherwise there is nothing to be colored!"
+            raise ValueError(msg)
+
+        max_value = int(np.max(space))
+
+        if gaps := (set(range(1, max_value + 1)) - set(np.unique(space[space > 0]))):
+            msg = f"Space must contain every value from 1 up to its max value! It has gaps at {gaps}."
+            raise ValueError(msg)
+
+        self._space_to_be_colored = space
+        self._colored_space = np.zeros_like(space, dtype=np.uint8)
+
+        if total_blocks_to_color is not None and total_blocks_to_color < max_value:
+            msg = "total_blocks_to_color must be at least as large as the current maximum value in the space!"
+            raise ValueError(msg)
+
+        self._total_blocks_to_color = total_blocks_to_color or max_value
+        self._next_block_to_color = 1
+        self._space_updated_callback = space_updated_callback
+
+        colors = list(range(1, self.NUM_COLORS + 1))
+        self._color_selection_iterable = (
+            RandomOffsetIterable(items=colors, seed=rng_seed) if use_rng else CyclingOffsetIterable(colors)
+        )
+
+        # self._uncolorable_block: int | None = None
+        self._finished = False
+
+    @property
+    def colored_space(self) -> NDArray[np.uint8]:
+        return self._colored_space
+
+    @property
+    def total_blocks_to_color(self) -> int:
+        return self._total_blocks_to_color
+
+    @property
+    def num_blocks_colored(self) -> int:
+        return self._next_block_to_color - 1
+
+    @contextlib.contextmanager
+    def _ensure_sufficient_recursion_depth(self) -> Iterator[None]:
+        required_depth = len(inspect.stack()) + self._total_blocks_to_color + self.STACK_FRAMES_SAFETY_MARGIN
+
+        prev_depth = sys.getrecursionlimit()
+        sys.setrecursionlimit(max(required_depth, prev_depth))
+        try:
+            yield
+        finally:
+            sys.setrecursionlimit(prev_depth)
+
+    def colorize(self) -> NDArray[np.uint8]:
+        """Fill up the space with tetrominos, each identified by a unique number, inplace."""
+        with self._ensure_sufficient_recursion_depth():
+            self._colorize()
+
+        assert self._finished, "Wasn't able to color the space with 4 colors, this should not be possible!"
+
+        return self._colored_space
+
+    def _colorize(self) -> None:
+        for _ in self._generate_coloring():
+            self._colorize()
+            if self._finished:
+                # when finished, simply unwind the stack all the way to the top, *without* backtracking
+                return
+        # at this point we either have tried everything to color `self._next_block_to_color` but were unsuccessful,
+        # or are in the process of fast backtracking because we have encountered an uncolorable block deeper down the
+        # stack
+
+        # # in case we are not already in the process of fast backtracking to a block we were unable to color
+        # if not self._uncolorable_block:
+        #     # remember this block as the block that could not be colored, then fast backtrack until a block close to it
+        #     # is uncolored, hopefully removing the issue that made this one uncolorable
+        #     self._uncolorable_block = self._next_block_to_color
+
+    def icolorize(self) -> Generator[None, None, NDArray[np.uint8]]:
+        """Fill up the space with tetrominos, each identified by a unique number, inplace."""
+        with self._ensure_sufficient_recursion_depth():
+            yield from self._icolorize()
+
+        assert self._finished, "Wasn't able to color the space with 4 colors, this should not be possible!"
+
+        return self._colored_space
+
+    def _icolorize(self) -> Iterator[None]:
+        for _ in self._generate_coloring():
+            yield
+
+            yield from self._icolorize()
+            if self._finished:
+                # when finished, simply unwind the stack all the way to the top, *without* backtracking
+                return
+
+            yield
+
+        # self._colored_space[self._space_to_be_colored == self._next_block_to_color] = 5
+        # yield
+
+    def _generate_coloring(self) -> Iterator[None]:
+        neighboring_colors, neighboring_uncolored_blocks = self._get_neighboring_colors_and_uncolored_blocks(
+            block=self._next_block_to_color
+        )
+
+        for color in self._color_selection_iterable:
+            if color in neighboring_colors:
+                continue
+
+            # if self._next_block_to_color == self._uncolorable_block:
+            #     self._uncolorable_block = None
+
+            self._colored_space[self._space_to_be_colored == self._next_block_to_color] = color
+            self._next_block_to_color += 1
+
+            # if (
+            #     self._uncolorable_block is not None
+            #     and len(self._get_neighboring_colors_and_uncolored_blocks(self._uncolorable_block)) == self.NUM_COLORS
+            # ):
+            #     continue
+
+            # x = False
+            # for uncolored_block in neighboring_uncolored_blocks:
+            #     if len(self._get_neighboring_colors_and_uncolored_blocks(uncolored_block)[0]) == self.NUM_COLORS:
+            #         # self._colored_space[self._space_to_be_colored == uncolored_block] = 6
+            #         self._next_block_to_color -= 1
+            #         self._colored_space[self._space_to_be_colored == self._next_block_to_color] = 0
+            #         x = True
+            #         break
+            # if x:
+            #     continue
+
+            if any(
+                len(self._get_neighboring_colors_and_uncolored_blocks(uncolored_block)[0]) == self.NUM_COLORS
+                for uncolored_block in neighboring_uncolored_blocks
+            ):
+                self._next_block_to_color -= 1
+                self._colored_space[self._space_to_be_colored == self._next_block_to_color] = 0
+                continue
+
+            if self._space_updated_callback is not None:
+                self._space_updated_callback()
+
+            if self._next_block_to_color > self._total_blocks_to_color:
+                self._finished = True
+                return
+
+            yield
+
+            self._next_block_to_color -= 1
+            self._colored_space[self._space_to_be_colored == self._next_block_to_color] = 0
+
+            if self._space_updated_callback is not None:
+                self._space_updated_callback()
+
+            # if self._uncolorable_block is not None and not self._blocks_are_adjacent(
+            #     self._next_block_to_color, self._uncolorable_block
+            # ):
+            #     # If the block we have just uncolored during backtracking is not adjacent to the uncolorable block, then
+            #     # this change has not made the uncolorable block colorable again, thus we need to immediately fast
+            #     # backtrack further.
+            #     return
+
+    def _get_neighboring_colors_and_uncolored_blocks(self, block: int) -> tuple[set[int], set[int]]:
+        neighboring_colors: set[int] = set()
+        neighboring_uncolored_blocks: set[int] = set()
+
+        block_positions = np.argwhere(self._space_to_be_colored == block)
+
+        for neighbor_position in chain.from_iterable(
+            block_positions + neighbor_offset for neighbor_offset in ((-1, 0), (0, -1), (1, 0), (0, 1))
+        ):
+            if (
+                not 0 <= neighbor_position[0] < self._colored_space.shape[0]
+                or not 0 <= neighbor_position[1] < self._colored_space.shape[1]
+                or self._space_to_be_colored[*neighbor_position] == block
+            ):
+                # neighbor is out of bounds, or is part of the block itself (not a "real" neighbor)
+                continue
+
+            if self._colored_space[*neighbor_position] != 0:
+                # neighbor is colored
+                neighboring_colors.add(int(self._colored_space[*neighbor_position]))
+            else:
+                # neighbor is uncolored
+                neighboring_uncolored_blocks.add(int(self._space_to_be_colored[*neighbor_position]))
+
+        return neighboring_colors, neighboring_uncolored_blocks
+
+    def _blocks_are_adjacent(self, block1: int, block2: int) -> bool:
+        assert block1 != block2
+
+        block1_positions = np.argwhere(self._space_to_be_colored == block1)
+        block2_positions = np.argwhere(self._space_to_be_colored == block2)
+
+        cell_distances = np.sum(np.abs(block1_positions - block2_positions))
+        return np.min(cell_distances) == 1
