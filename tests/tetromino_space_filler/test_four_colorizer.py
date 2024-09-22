3ec3d55b4e180f025e42e0a3d2d0bc72c6b8cbc3
Florian Bender <benderflorian70@gmail.com>
2024-09-22 11:51:38 +0200

    Add FourColorizer 'integration test'

--- tests/tetromino_space_filler/test_four_colorizer.py
+++ tests/tetromino_space_filler/test_four_colorizer.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.typing import NDArray
 
 from tetris.tetromino_space_filler.four_colorizer import FourColorizer
 
@@ -56,3 +57,48 @@ def test_get_neighboring_colors_and_uncolored_blocks() -> None:
         ]
     )
     assert colorizer._get_neighboring_colors_and_uncolored_blocks(5) == ({1, 2}, {3})
+
+
+def test_colorize() -> None:
+    # fmt: off
+    space = np.array(
+        [
+            [ 1,  2,  2,  2,  7,  7,  7,  7, 25, 25],
+            [ 1,  3,  2,  6,  6,  6,  6,  8, 24, 25],
+            [ 1,  3,  3,  5,  5,  5,  9,  8, 24, 25],
+            [ 1,  3,  4,  4,  5, 10,  9,  8, 24, 23],
+            [15, 15,  4,  4, 11, 10,  9,  8, 24, 23],
+            [15, 14, 14, 11, 11, 10,  9, 21, 21, 23],
+            [15, 14, 13, 11, 12, 10, 18, 20, 21, 23],
+            [16, 14, 13, 13, 12, 18, 18, 20, 21, 22],
+            [16, 17, 17, 13, 12, 18, 20, 20, 22, 22],
+            [16, 16, 17, 17, 12, 19, 19, 19, 19, 22],
+        ],
+        dtype=np.int32,
+    )
+    # fmt: on
+    colored_space = FourColorizer(space).colorize()
+
+    colors = np.unique(colored_space)
+    assert set(colors) <= {1, 2, 3, 4}
+
+    for block_index in np.unique(space):
+        block_colors = np.unique(colored_space[space == block_index])
+        assert len(block_colors) == 1, f"Block {block_index} has multiple colors ({block_colors.tolist()})"
+
+        block_color = block_colors[0]
+        assert not any(
+            colored_space[neighbor_position] == block_color
+            for neighbor_position in _neighboring_positions(space, block_index)
+        ), f"Block {block_index} has a neighbor with the same color ({block_color})"
+
+
+def _neighboring_positions(space: NDArray[np.int32], block_index: int) -> set[tuple[int, int]]:
+    block_positions = np.argwhere(space == block_index)
+    return {
+        tuple(neighbor_position)
+        for offset in ((-1, 0), (0, -1), (1, 0), (0, 1))
+        for neighbor_position in block_positions + offset
+        if neighbor_position not in block_positions
+        and (0 <= neighbor_position[0] < space.shape[0] and 0 <= neighbor_position[1] < space.shape[1])
+    }
