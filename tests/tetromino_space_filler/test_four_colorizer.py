dd021402084ab6843df65cb5cdf0151e7b70259c
Florian Bender <benderflorian70@gmail.com>
2024-09-22 11:07:55 +0200

    Fix and add FourColorizer tests

--- tests/tetromino_space_filler/test_four_colorizer.py
+++ tests/tetromino_space_filler/test_four_colorizer.py
@@ -3,7 +3,7 @@ import numpy as np
 from tetris.tetromino_space_filler.four_colorizer import FourColorizer
 
 
-def test_adjacent_blocks() -> None:
+def test_blocks_are_close_adjacent_blocks() -> None:
     space = np.array(
         [
             [1, 1, 0],
@@ -11,10 +11,11 @@ def test_adjacent_blocks() -> None:
             [0, 0, 0],
         ]
     )
-    assert FourColorizer._blocks_are_close(space, 1, 2)
+    assert FourColorizer(space)._blocks_are_close(1, 2)
+    assert FourColorizer(space, closeness_threshold=1)._blocks_are_close(1, 2)
 
 
-def test_non_adjacent_blocks() -> None:
+def test_blocks_are_close_non_adjacent_blocks() -> None:
     space = np.array(
         [
             [1, 1, 0],
@@ -22,10 +23,11 @@ def test_non_adjacent_blocks() -> None:
             [0, 2, 2],
         ]
     )
-    assert not FourColorizer._blocks_are_close(space, 1, 2)
+    assert FourColorizer(space)._blocks_are_close(1, 2)
+    assert not FourColorizer(space, closeness_threshold=1)._blocks_are_close(1, 2)
 
 
-def test_diagonally_adjacent_blocks() -> None:
+def test_blocks_are_close_diagonally_adjacent_blocks() -> None:
     space = np.array(
         [
             [1, 0, 0],
@@ -33,4 +35,24 @@ def test_diagonally_adjacent_blocks() -> None:
             [0, 0, 0],
         ]
     )
-    assert not FourColorizer._blocks_are_close(space, 1, 2)
+    assert FourColorizer(space)._blocks_are_close(1, 2)
+    assert not FourColorizer(space, closeness_threshold=1)._blocks_are_close(1, 2)
+
+
+def test_get_neighboring_colors_and_uncolored_blocks() -> None:
+    space = np.array(
+        [
+            [1, 1, 0, 2],
+            [0, 5, 5, 2],
+            [3, 3, 4, 4],
+        ]
+    )
+    colorizer = FourColorizer(space)
+    colorizer._colored_space = np.array(
+        [
+            [1, 1, 0, 1],
+            [0, 0, 0, 1],
+            [0, 0, 2, 2],
+        ]
+    )
+    assert colorizer._get_neighboring_colors_and_uncolored_blocks(5) == ({1, 2}, {3})
