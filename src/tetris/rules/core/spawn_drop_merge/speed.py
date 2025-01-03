99ed25e1193561174ef93df293a5dabc0b4a513f
Florian Bender <benderflorian70@gmail.com>
2025-01-03 15:02:55 +0100

    Allow for float-valued drop/merge intervals

--- src/tetris/rules/core/spawn_drop_merge/speed.py
+++ src/tetris/rules/core/spawn_drop_merge/speed.py
@@ -1,14 +1,52 @@
-from typing import NamedTuple
+from collections.abc import Iterator
+from math import ceil, floor
+from typing import NamedTuple, Self
 
 from tetris.game_logic.interfaces.callback import Callback
 from tetris.game_logic.interfaces.rule import Subscriber
 from tetris.rules.core.clear_full_lines_rule import LineClearMessage
 
 
+class IntIterWithFloatAverage(Iterator[int]):
+    """An iterator that yields integers, just above or below the specified target average float.
+
+    The average of the values yielded by this iterator will converge towards the target average.
+    """
+
+    def __init__(self, target_average: float) -> None:
+        self._target_average = target_average
+        self.reset()
+
+    def reset(self) -> None:
+        self._num_values = 0
+        self._current_average: float | None = None
+
+    @property
+    def target_average(self) -> float:
+        return self._target_average
+
+    @target_average.setter
+    def target_average(self, target_average: float) -> None:
+        self._target_average = target_average
+        self.reset()
+
+    def __next__(self) -> int:
+        overshot = (self._current_average or 0) > self._target_average
+        next_value = floor(self._target_average) if overshot else ceil(self._target_average)
+
+        self._current_average = ((self._current_average or 0) * self._num_values + next_value) / (self._num_values + 1)
+        self._num_values += 1
+
+        return next_value
+
+    def __iter__(self) -> Self:
+        return self
+
+
 class SpeedStrategy(Callback):
     """Strategy for whether to drop/merge the current block, given the number of frames since it was spawned."""
 
-    def __init__(self, base_interval: int = 25, quick_interval_factor: float = 8) -> None:
+    def __init__(self, base_interval: float = 25, quick_interval_factor: float = 8) -> None:
         """Initialize the SpeedStrategy.
 
         Needs to be registered as a callback to the game in order to work correctly.
@@ -23,20 +61,39 @@ class SpeedStrategy(Callback):
         self._base_interval = base_interval
         self._quick_interval_factor = quick_interval_factor
 
+        self._normal_interval_iter = IntIterWithFloatAverage(self._base_interval)
+        self._quick_interval_iter = IntIterWithFloatAverage(max(self._base_interval / self._quick_interval_factor, 1))
+
         self._set_interval(base_interval)
 
-    def _set_interval(self, normal_interval: int) -> None:
+    def _set_interval(self, normal_interval: float) -> None:
         self._normal_interval = normal_interval
-        self._quick_interval = max(round(normal_interval / self._quick_interval_factor), 1)
+        self._quick_interval = max(normal_interval / self._quick_interval_factor, 1)
+
+        self._normal_interval_iter.target_average = self._normal_interval
+        self._quick_interval_iter.target_average = self._quick_interval
+
+        self._next_normal_interval_int = next(self._normal_interval_iter)
+        self._next_quick_interval_int = next(self._quick_interval_iter)
 
     def on_game_start(self) -> None:
         self._set_interval(self._base_interval)
 
     def should_trigger(self, frames_since_last_drop: int, *, quick_drop_held: bool) -> bool:
         if quick_drop_held:
-            return frames_since_last_drop >= self._quick_interval
+            should_trigger = frames_since_last_drop >= self._next_quick_interval_int
 
-        return frames_since_last_drop >= self._normal_interval
+            if should_trigger:
+                self._next_quick_interval_int = next(self._quick_interval_iter)
+
+            return should_trigger
+
+        should_trigger = frames_since_last_drop >= self._next_normal_interval_int
+
+        if should_trigger:
+            self._next_normal_interval_int = next(self._normal_interval_iter)
+
+        return should_trigger
 
 
 class LineClearSpeedupStrategy(SpeedStrategy, Subscriber):
@@ -48,10 +105,10 @@ class LineClearSpeedupStrategy(SpeedStrategy, Subscriber):
 
     def __init__(
         self,
-        base_interval: int = 25,
+        base_interval: float = 25,
         quick_interval_factor: float = 8,
         *,
-        minimum_normal_interval: int = 8,
+        minimum_normal_interval: float = 4,
         speedup_factor: float = 1.1,
         line_clears_between_speedup: int = 10,
     ) -> None:
@@ -75,7 +132,6 @@ class LineClearSpeedupStrategy(SpeedStrategy, Subscriber):
 
         self._minimum_normal_interval = minimum_normal_interval
         self._speedup_factor = speedup_factor
-        self._float_interval: float = self._normal_interval
 
     def notify(self, message: NamedTuple) -> None:
         if not isinstance(message, LineClearMessage) or self._normal_interval <= self._minimum_normal_interval:
@@ -83,14 +139,11 @@ class LineClearSpeedupStrategy(SpeedStrategy, Subscriber):
 
         self._cleared_lines += len(message.cleared_lines)
 
-        if self._cleared_lines >= self._line_clears_between_speedup:
-            self._float_interval = self._normal_interval / self._speedup_factor
-            self._set_interval(max(round(self._float_interval), self._minimum_normal_interval))
-
+        while self._cleared_lines >= self._line_clears_between_speedup:
+            self._set_interval(max(self._normal_interval / self._speedup_factor, self._minimum_normal_interval))
             self._cleared_lines -= self._line_clears_between_speedup
 
     def on_game_start(self) -> None:
         super().on_game_start()
 
         self._cleared_lines = 0
-        self._float_interval = self._normal_interval
