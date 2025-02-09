97539be9a1e8d5a54253363ef43e579090004582
Florian Bender <benderflorian70@gmail.com>
2025-02-09 01:48:51 +0100

    Fix division by zero bug

--- src/tetris/ui/cli/ui.py
+++ src/tetris/ui/cli/ui.py
@@ -175,7 +175,7 @@ class CLI(UI):
         num_rows = ceil(num_boards / num_cols)
 
         # reduce the number of columns, as long as this doesn't increase the required number of rows
-        while ceil(num_boards / (num_cols - 1)) == num_rows:
+        while num_cols > 1 and ceil(num_boards / (num_cols - 1)) == num_rows:
             num_cols -= 1
 
         return num_rows, num_cols
