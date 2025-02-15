b133b5458762387450b1ba3da7738950efb352af
Florian Bender <benderflorian70@gmail.com>
2025-02-15 21:45:35 +0100

    Refactor HeuristicBotGym and GeneticAlgorithm
    - population and fitnesses are sorted within GeneticAlgorithm
    - (sorted) fitnesses are saved in checkpoint
    - when provied with fitnesses, genetic algorithm initially does evolve step before first evalaution
    - elitism_rate = float("inf") will just select the fittest N individuals
    - HeuristicBotGym holds (and saves in checkpoint) GeneticAlgorithm instance, which in turn holds mutator, survival_rate, and elitism_factor, and is now a dataclass

--- src/tetris/genetic_algorithm.py
+++ src/tetris/genetic_algorithm.py
@@ -1,78 +1,112 @@
 import logging
 from collections.abc import Callable
-from itertools import count, cycle
+from dataclasses import dataclass
+from itertools import count, cycle, islice
 
 import numpy as np
 
 LOGGER = logging.getLogger(__name__)
 
 
+@dataclass(slots=True)
 class GeneticAlgorithm[T]:
-    def __init__(  # noqa: PLR0913
+    mutator: Callable[[T], T]
+    survival_rate: float = 0.5
+    elitism_factor: float = 1.0
+
+    def run(
         self,
-        initial_population: list[T],
+        population: list[T],
         population_evaluator: Callable[[list[T]], list[float]],
-        mutator: Callable[[T], T],
+        fitnesses: list[float] | None = None,
         *,
+        num_generations: int | None = None,
         post_evaluation_callback: Callable[[list[T], list[float]], None] | None = None,
-        survival_rate: float = 0.5,
-        elitism_factor: float = 1.0,
     ) -> None:
-        self._population = initial_population
-        self._population_evaluator = population_evaluator
-        self._mutator = mutator
-        self._post_evaluation_callback = post_evaluation_callback
+        if fitnesses:
+            if len(fitnesses) != len(population):
+                msg = (
+                    "`fitnesses` must have same length as `population`, "
+                    f"got {len(fitnesses) = } vs. {len(population) = }"
+                )
+                raise ValueError(msg)
 
-        self._survival_rate = survival_rate
-        self._elitism_factor = elitism_factor
+            LOGGER.debug("Fitnesses provided, evolving before first evaluation...")
+            # if we have been provided fitnesses of the initial population (e.g. saved from a previous run),
+            # immediately use them to evolve the initial population
+            sorted_population, sorted_fitnesses = self._to_sorted(population=population, fitnesses=fitnesses)
+            population = self._evolve(sorted_population=sorted_population, sorted_fitnesses=sorted_fitnesses)
 
-    def run(self, num_generations: int | None = None) -> None:
         for i in range(num_generations) if num_generations is not None else count():
             LOGGER.info("Evaluating generation %d", i)
 
-            fitnesses = self._population_evaluator(self._population)
+            fitnesses = population_evaluator(population)
 
-            if self._post_evaluation_callback is not None:
-                self._post_evaluation_callback(self._population, fitnesses)
+            sorted_population, sorted_fitnesses = self._to_sorted(population=population, fitnesses=fitnesses)
 
-            self._evolve(fitnesses)
+            if post_evaluation_callback is not None:
+                post_evaluation_callback(sorted_population, sorted_fitnesses)
 
-    def _evolve(self, fitnesses: list[float]) -> None:
-        """Evolve the population by selecting survivors and mutating them.
+            population = self._evolve(sorted_population=sorted_population, sorted_fitnesses=sorted_fitnesses)
 
-        Expects self._population to be sorted by fitness.
+    @staticmethod
+    def _to_sorted(population: list[T], fitnesses: list[float]) -> tuple[list[T], list[float]]:
+        sorted_population, sorted_fitnesses = zip(
+            *sorted(
+                zip(population, fitnesses, strict=True),
+                key=lambda x: x[1],
+                reverse=True,
+            ),
+            strict=True,
+        )
+        return list(sorted_population), list(sorted_fitnesses)
+
+    def _evolve(self, sorted_population: list[T], sorted_fitnesses: list[float]) -> list[T]:
+        """Evolve the population by selecting survivors and mutating them, returning the evolved population."""
+        population_size = len(sorted_population)
+        assert len(sorted_fitnesses) == population_size
+
+        survivors = self._select_survivors(sorted_population, sorted_fitnesses)
+        return self.expanded_population_through_mutation(population=survivors, new_size=population_size)
+
+    def expanded_population_through_mutation(self, population: list[T], new_size: int) -> list[T]:
+        """Return a population expanded to the desired size.
+
+        Mutated versions of the existing individuals in the populations are used for filling the list up to the desired
+        size.
         """
-        population_size = len(self._population)
-        assert len(fitnesses) == population_size
+        assert new_size >= len(population)
+        new_population = population.copy()
+        new_population.extend(
+            self.mutator(individual) for individual in islice(cycle(population), new_size - len(population))
+        )
+        return new_population
 
-        survivors = self._select_survivors(fitnesses)
-        self._population = survivors
-
-        for i in cycle(range(len(survivors))):
-            if len(self._population) >= population_size:
-                break
-
-            self._population.append(self._mutator(survivors[i]))
-
-    def _select_survivors(self, fitnesses: list[float]) -> list[T]:
+    def _select_survivors(self, sorted_population: list[T], sorted_fitnesses: list[float]) -> list[T]:
         """Select survivors from a sorted population using weighted sampling.
 
         Args:
-            fitnesses: Fitness values of the individuals in the population.
+            sorted_population: The population of individuals, sorted by fitness.
+            sorted_fitnesses: Fitness values of the individuals in the population, sorted.
             self._survival_rate: Fraction of the population that should survive. Must be in the range [0, 1].
             self._elitism_factor: How much to favor the best individuals.
                 A factor of 0 means that all individuals have the same chance of surviving.
-                A very high factor (approaching infinity) means that exactly the best
-                `round(survival_rate * len(self._population))` individuals survive.
+                A very factor of `inf` means that exactly the best `round(survival_rate * len(self._population))`
+                individuals survive.
+                A factor of < 0 is not recommended and will lead to less fit individuals having a higher chance of
+                surviving.
         """
-        population_size = len(self._population)
-        num_survivors = min(max(1, round(population_size * self._survival_rate)), population_size)
+        population_size = len(sorted_population)
+        num_survivors = min(max(1, round(population_size * self.survival_rate)), population_size)
+
+        if self.elitism_factor == float("inf"):
+            return sorted_population[:num_survivors]
 
         # individuals with higher fitness have a higher weight (chance of surviving)
-        weights = np.array(fitnesses, dtype=float)
+        weights = np.array(sorted_fitnesses, dtype=float)
         # ensure all weights are > 0
         weights -= min(np.min(weights) - 1e-10, 0)
-        weights **= self._elitism_factor
+        weights **= self.elitism_factor
 
         probabilities = weights / weights.sum()
 
@@ -80,4 +114,4 @@ class GeneticAlgorithm[T]:
             population_size, size=num_survivors, replace=False, p=probabilities
         )
 
-        return [self._population[i] for i in selected_indices]
+        return [sorted_population[i] for i in sorted(selected_indices)]
--- src/tetris/heuristic_bot_gym/heuristic_bot_gym.py
+++ src/tetris/heuristic_bot_gym/heuristic_bot_gym.py
@@ -1,11 +1,12 @@
 import logging
 import pickle
 from abc import ABC, abstractmethod
-from collections.abc import Callable, Sequence
+from collections.abc import Sequence
+from dataclasses import asdict
 from datetime import UTC, datetime
-from itertools import cycle, islice
 from pathlib import Path
 from pprint import pformat
+from types import LambdaType
 from typing import Any, BinaryIO
 
 from tetris import logging_config
@@ -20,7 +21,7 @@ LOGGER = logging.getLogger(__name__)
 def main() -> None:
     logging_config.configure_logging()
 
-    HeuristicGym.continue_from_latest_checkpoint()
+    HeuristicGym.continue_from_latest_checkpoint(checkpoint_dir=Path(__file__).parent / ".checkpoints" / "15x10")
 
 
 class Evaluator(ABC):
@@ -38,16 +39,21 @@ class HeuristicGym:
     def __init__(
         self,
         population_size: int = 100,
-        evaluator: Evaluator | None = None,
         *,
-        mutator: Callable[[Heuristic], Heuristic] = mutated_heuristic,
+        evaluator: Evaluator | None = None,
+        genetic_algorithm: GeneticAlgorithm[Heuristic] | None = None,
         checkpoint_dir: Path | None = Path(__file__).parent / ".checkpoints",
     ) -> None:
+        if genetic_algorithm and checkpoint_dir and isinstance(genetic_algorithm.mutator, LambdaType):
+            msg = (
+                "Can't save checkpoints when using a lambda function as mutator - use functools.partial or def instead!"
+            )
+            raise ValueError(msg)
+
         self._population_size = population_size
-
         self._evaluator = evaluator or self._default_evaluator()
+        self._genetic_algorithm = genetic_algorithm or GeneticAlgorithm(mutator=mutated_heuristic)
 
-        self._mutator = mutator
         self._checkpoint_dir = checkpoint_dir
         if self._checkpoint_dir:
             self._checkpoint_dir.mkdir(parents=True, exist_ok=True)
@@ -58,39 +64,34 @@ class HeuristicGym:
 
         return ParallelWithinBotEvaluator()
 
-    def run(self, initial_population: Sequence[Heuristic] | None = None, num_generations: int | None = None) -> None:
-        initial_population = list(initial_population) if initial_population is not None else [Heuristic()]  # type: ignore[call-arg]
+    def run(
+        self,
+        initial_population: list[Heuristic] | None = None,
+        initial_population_fitnesses: list[float] | None = None,
+        num_generations: int | None = None,
+    ) -> None:
+        initial_population = initial_population or [Heuristic()]  # type: ignore[call-arg]
 
         if len(initial_population) > self._population_size:
             initial_population = initial_population[: self._population_size]
+            if initial_population_fitnesses:
+                initial_population_fitnesses = initial_population_fitnesses[: self._population_size]
+
         elif len(initial_population) < self._population_size:
-            initial_population = self._expanded_population(initial_population, new_size=self._population_size)
+            initial_population = self._genetic_algorithm.expanded_population_through_mutation(
+                population=initial_population, new_size=self._population_size
+            )
+            initial_population_fitnesses = None
 
-        GeneticAlgorithm(
-            initial_population=initial_population,
+        self._genetic_algorithm.run(
+            population=initial_population,
             population_evaluator=self._evaluator,
-            mutator=self._mutator,
+            fitnesses=initial_population_fitnesses,
+            num_generations=num_generations,
             post_evaluation_callback=self._post_evaluation_callback,
-        ).run(num_generations)
-
-    def _expanded_population(self, population: list[Heuristic], new_size: int) -> list[Heuristic]:
-        assert new_size > len(population)
-        new_population = population.copy()
-        new_population.extend(
-            self._mutator(heuristic) for heuristic in islice(cycle(population), new_size - len(population))
-        )
-        return new_population
-
-    def _post_evaluation_callback(self, population: list[Heuristic], fitnesses: list[float]) -> None:
-        sorted_population, sorted_fitnesses = zip(
-            *sorted(
-                zip(population, fitnesses, strict=True),
-                key=lambda x: x[1],
-                reverse=True,
-            ),
-            strict=True,
         )
 
+    def _post_evaluation_callback(self, sorted_population: list[Heuristic], sorted_fitnesses: list[float]) -> None:
         LOGGER.info("Sorted fitnesses: %s", sorted_fitnesses)
         LOGGER.info("Best heuristic '%r' scored %d", sorted_population[0], sorted_fitnesses[0])
 
@@ -98,20 +99,22 @@ class HeuristicGym:
 
         if self._checkpoint_dir:
             self._save_checkpoint(
-                sorted_population,
-                (
+                population=sorted_population,
+                fitnesses=sorted_fitnesses,
+                file=(
                     self._checkpoint_dir
                     / datetime.now(UTC).strftime(f"%Y-%m-%d_%H-%M-%S_fitness-{sorted_fitnesses[0]}.pkl")
                 ).open("wb"),
             )
 
-    def _save_checkpoint(self, population: Sequence[Heuristic], file: BinaryIO) -> None:
+    def _save_checkpoint(self, population: Sequence[Heuristic], fitnesses: list[float], file: BinaryIO) -> None:
         pickle.dump(
             {
                 "population": population,
-                "mutator": self._mutator,
+                "fitnesses": fitnesses,
                 "evaluator_type": type(self._evaluator),
                 "evaluator_config": self._evaluator.config,
+                "genetic_algorithm_config": asdict(self._genetic_algorithm),
             },
             file,
         )
@@ -121,22 +124,30 @@ class HeuristicGym:
         cls,
         checkpoint_dir: Path = Path(__file__).parent / ".checkpoints",
         new_population_size: int | None = None,
+        num_generations: int | None = None,
         **kwargs_to_overwrite: Any,  # noqa: ANN401
     ) -> None:
         checkpoint_files_sorted = sorted(checkpoint_dir.iterdir(), key=lambda file: file.stat().st_mtime, reverse=True)
         latest_checkpoint_file = next(
-            (checkpoint_file for checkpoint_file in checkpoint_files_sorted if checkpoint_file.is_file()), None
+            (
+                checkpoint_file
+                for checkpoint_file in checkpoint_files_sorted
+                if checkpoint_file.is_file() and checkpoint_file.suffix == ".pkl"
+            ),
+            None,
         )
 
         if latest_checkpoint_file is None:
             msg = f"checkpoint_dir '{checkpoint_dir}' contains no checkpoint files!"
             raise ValueError(msg)
-        LOGGER.debug(latest_checkpoint_file)
+
+        LOGGER.debug("Loading checkpoint from '%s'", latest_checkpoint_file)
 
         cls.continue_from_checkpoint(
             latest_checkpoint_file.open("rb"),
             checkpoint_dir=checkpoint_dir,
             new_population_size=new_population_size,
+            num_generations=num_generations,
             **kwargs_to_overwrite,
         )
 
@@ -146,19 +157,34 @@ class HeuristicGym:
         checkpoint_file: BinaryIO,
         checkpoint_dir: Path | None = Path(__file__).parent / ".checkpoints",
         new_population_size: int | None = None,
+        num_generations: int | None = None,
         **kwargs_to_overwrite: Any,  # noqa: ANN401
     ) -> None:
         # make sure to only load from trusted sources
         checkpoint: dict[str, Any] = deep_merge(pickle.load(checkpoint_file), kwargs_to_overwrite)  # noqa: S301
 
         population: list[Heuristic] = checkpoint.pop("population")
-        checkpoint["population_size"] = new_population_size or len(population)
+        fitnesses: list[float] | None = checkpoint.pop("fitnesses", None)
+
+        if fitnesses:
+            LOGGER.debug("Loaded fitnesses %s", fitnesses)
+
+        population_size = new_population_size or len(population)
 
         LOGGER.info("Continuing HeuristicBotGym with config\n%s", pformat(checkpoint))
 
         checkpoint["evaluator"] = checkpoint.pop("evaluator_type")(**checkpoint.pop("evaluator_config"))
+        if genetic_algorithm_config := checkpoint.pop("genetic_algorithm_config", None):
+            checkpoint["genetic_algorithm"] = GeneticAlgorithm(**genetic_algorithm_config)
+        elif mutator := checkpoint.pop("mutator", None):
+            # support for legacy checkpoint format
+            checkpoint["genetic_algorithm"] = GeneticAlgorithm(mutator=mutator)
 
-        cls(checkpoint_dir=checkpoint_dir, **checkpoint).run(initial_population=population)
+        cls(population_size=population_size, checkpoint_dir=checkpoint_dir, **checkpoint).run(
+            initial_population=population,
+            initial_population_fitnesses=fitnesses,
+            num_generations=num_generations,
+        )
 
 
 if __name__ == "__main__":
